743d329cf5238815cfb3bbcef408529106ecda22
SeungKi Kim <tttuuu888@gmail.com>
2021-06-01 23:08:42 +0900

    Add copyright

--- package-loading-notifier.el
+++ package-loading-notifier.el
@@ -1,5 +1,7 @@
 ;;; package-loading-notifier.el --- Notify a package is being loaded -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2021 SeungKi Kim
+
 ;; Author: SeungKi Kim <tttuuu888@gmail.com>
 ;; URL: https://github.com/tttuuu888/package-loading-notifier
 ;; Version: 0.1.0
